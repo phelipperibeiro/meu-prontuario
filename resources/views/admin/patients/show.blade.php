b915583fa7d0d4834fe15bde7afb162dce8af4e7
Felipe Ribeiro <felipe.ribeiro@skymail.com.br>
2020-11-02 17:29:57 -0300

    adicionando botão de cadastro de exames

--- resources/views/admin/patients/show.blade.php
+++ resources/views/admin/patients/show.blade.php
@@ -1,6 +1,15 @@
 @extends('layouts.admin')
 @section('content')
 
+@can('patient_create')
+    <div style="margin-bottom: 10px;" class="row">
+        <div class="col-lg-12">
+            <a class="btn btn-success" href="{{ route("admin.patient-medical-exams.create", ['id'=> $patient->id]) }}">
+                {{ trans('global.add') }} {{ trans('global.patient-medical-exams.title_singular') }}
+            </a>
+        </div>
+    </div>
+@endcan
 <div class="card">
     <div class="card-header">
         {{ trans('global.show') }} {{ trans('global.patient.title') }}
