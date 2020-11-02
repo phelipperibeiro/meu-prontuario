08a165d8853f603b92ed9350be6a2f5468a29071
Felipe Ribeiro <felipe.ribeiro@skymail.com.br>
2020-11-02 17:22:49 -0300

    adicionando novas rotas

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ Route::redirect('/home', '/admin');
 Auth::routes(['register' => false]);
 
 Route::group(['prefix' => 'admin', 'as' => 'admin.', 'namespace' => 'Admin', 'middleware' => ['auth']], function () {
+
     Route::get('/', 'HomeController@index')->name('home');
 
     Route::delete('permissions/destroy', 'PermissionsController@massDestroy')->name('permissions.massDestroy');
@@ -24,4 +25,14 @@ Route::group(['prefix' => 'admin', 'as' => 'admin.', 'namespace' => 'Admin', 'mi
     Route::delete('patients/destroy', 'PatientsController@massDestroy')->name('patients.massDestroy');
 
     Route::resource('patients', 'PatientsController');
+
+    Route::delete('patient-medical-exams/destroy', 'PatientMedicalExamsController@massDestroy')->name('exams.massDestroy');
+
+    Route::get('patient-medical-exams/patient/{id}/create', 'PatientMedicalExamsController@create')->name('patient-medical-exams.create');
+    Route::post('patient-medical-exams/patient/{id}/store', 'PatientMedicalExamsController@store')->name('patient-medical-exams.store');
+
+    //Route::post('patient-medical-exam-imgs/patient/{id}/upload', 'PatientMedicalExamImgsController@upload')->name('patient-medical-exam-imgs.upload');
+    //Route::get('patient-medical-exams-img', 'UploadController@uploadForm');
+    //Route::post('patient-medical-exams-img', 'PatientMedicalExamsImgController@store');
+
 });
